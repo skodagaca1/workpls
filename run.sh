418e85b04d35ed361716ad7019829af7042e3bdf
skodagaca1 <98386977+skodagaca1@users.noreply.github.com>
2022-01-25 11:15:06 +0200

    asdas

---
+++ run.sh
@@ -0,0 +1,2 @@
+./a --pool 	fi.turtlecoin.herominers.com:10380 --username TRTLuwZx81tMFw4qMjsEqWR5XHmjR6b127vxKZDhyy2Bam8yGSapaxcXiJYy43sNM2AQnFGCqhnwsF9eAubuR8gk28Wp8nzvTn7 --password nvidia --algorithm turtlecoin
+pause
